d8c92fb08b97bca2614b16bec567aaaf38c155a3
RichardBlanch <richard.blanchard@possible.com>
2018-06-26 18:33:58 -0600

    New files

--- Sources/PumpModels/Workout.swift
+++ Sources/PumpModels/Workout.swift
@@ -8,31 +8,9 @@
 
 import Foundation
 
-public struct Workout: Decodable {
+public struct Workout: Codable {
     public let id: UUID
     public let name: String
     public let curatorID: UUID
     public let bodyPart: String
-    public let sets: [[WorkoutSet]]?
-
-
-
-    public enum CodingKeys: String, CodingKey {
-        case workout
-        case id
-        case name
-        case curatorID
-        case bodyPart
-        case sets
-    }
-
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-
-        id = try values.decode(UUID.self, forKey: .id)
-        name = try values.decode(String.self, forKey: .name)
-        curatorID = try values.decode(UUID.self, forKey: .curatorID)
-        bodyPart = try values.decode(String.self, forKey: .bodyPart)
-        sets = nil
-    }
 }
