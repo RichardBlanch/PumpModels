18149e82ce5d2cc66894e7fdb64aacb6fdddd711
RichardBlanch <richard.blanchard@possible.com>
2018-06-28 21:03:27 -0600

    Workout decoder update

--- Sources/PumpModels/Workout.swift
+++ Sources/PumpModels/Workout.swift
@@ -13,11 +13,52 @@ public struct Workout: Codable {
     public var name: String
     public var curatorID: UUID
     public var bodyPart: String
+    public let sets: [[WorkoutSet]]
 
     public init(id: UUID, name: String, bodyPart: String, curatorID: UUID) {
         self.id = id
         self.name = name
         self.bodyPart = bodyPart
         self.curatorID = curatorID
+        self.sets = []
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case workout
+        case id
+        case name
+        case curatorID
+        case bodyPart
+    }
+
+    enum WorkoutPayloadCodingKeys: String, CodingKey {
+        case id
+        case name
+        case curatorID
+        case bodyPart
+        case sets
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        if let workoutContainer = try? container.nestedContainer(keyedBy: WorkoutPayloadCodingKeys.self, forKey: .workout) {
+            id = try workoutContainer.decode(UUID.self, forKey: WorkoutPayloadCodingKeys.id)
+            name = try workoutContainer.decode(String.self, forKey: WorkoutPayloadCodingKeys.name)
+            bodyPart = try workoutContainer.decode(String.self, forKey: WorkoutPayloadCodingKeys.bodyPart)
+            curatorID = try workoutContainer.decode(UUID.self, forKey: WorkoutPayloadCodingKeys.curatorID)
+            sets = try workoutContainer.decode([[WorkoutSet]].self, forKey: WorkoutPayloadCodingKeys.sets)
+
+        } else {
+            id = try container.decode(UUID.self, forKey: CodingKeys.id)
+            name = try container.decode(String.self, forKey: CodingKeys.name)
+            bodyPart = try container.decode(String.self, forKey: CodingKeys.bodyPart)
+            curatorID = try container.decode(UUID.self, forKey: CodingKeys.curatorID)
+            sets = []
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        fatalError()
     }
 }
